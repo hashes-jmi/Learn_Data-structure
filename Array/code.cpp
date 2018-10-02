44f2d6f5da05912cfe28fb1e5fbbd3bf9d9941bd
prathambatra <batrapratham999@gmail.com>
2018-10-02 12:19:13 +0530

    array-code
    Signed-off-by: prathambatra <batrapratham999@gmail.com>

---
+++ Array/code.cpp
@@ -0,0 +1,13 @@
+#include<bit/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int size,arr[100]; // Declaration(Static)
+
+	cin>>size;
+	for(int i=0;i<size;i++)  //Taking input
+		cin>>arr[i];
+	for(int i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+}
